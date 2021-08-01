5c5565751f672047c7eea12d93dce1f7dc8d79ad
Deepanki Jain <deepankijain@gmail.com>
2021-08-02 00:16:58 +0530

    Change port

--- server.js
+++ server.js
@@ -33,4 +33,4 @@ io.on('connection', (socket) => {
     })
   })
 })
-server.listen(3000, () => console.log('Server is running on port 3000!'))
+server.listen(80, () => console.log('Server is running on port 3000!'))
